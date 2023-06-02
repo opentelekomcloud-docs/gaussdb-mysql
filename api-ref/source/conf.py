ff50f1485ca4299fada354aa8767b388707bbfad
Tino Schr <tino.schreiber@t-systems.com>
2023-06-02 11:53:54 +0200

    Update tox.ini && conf.py file (#23)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: SebastianGode

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -28,6 +28,15 @@ project = 'GaussDB (for MySQL)'
 otcdocs_repo_name = 'opentelekomcloud-docs/gaussdb-mysql'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/gaussdb-mysql/api-ref/'
+otcdocs_doc_title = 'API Reference'
+otcdocs_doc_type = 'api-ref'
+otcdocs_service_category = 'database'
+otcdocs_service_title = 'GaussDB (for MySQL)'
+otcdocs_service_type = 'gaussdb_mysql'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
